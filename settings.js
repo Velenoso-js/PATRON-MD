bcfda38903a07b66921c6698f7e623c03d59a697
Velenoso-js <heisvelenoso@gmail.com>
2025-01-04 12:34:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "9019928426 "
 
-global.bankowner = "FADARE"
+global.bankowner = "ADEMOLA"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
